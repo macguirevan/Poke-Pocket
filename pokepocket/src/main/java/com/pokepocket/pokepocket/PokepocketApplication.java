ea32f1dce46a33a85bb88bbc53286a012b78fa4b
MatthewJung14 <Matthewjung14@gmail.com>
2025-03-10 20:26:07 -0400

    Springboot Initialization

---
+++ pokepocket/src/main/java/com/pokepocket/pokepocket/PokepocketApplication.java
@@ -0,0 +1,13 @@
+package com.pokepocket.pokepocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokepocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokepocketApplication.class, args);
+	}
+
+}
