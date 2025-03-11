30187156fef17e084188171a53bea5737514aeec
MatthewJung14 <Matthewjung14@gmail.com>
2025-03-10 22:08:53 -0400

    Created user table

--- pokepocket/src/main/java/com/pokepocket/PokepocketApplication.java
+++ pokepocket/src/main/java/com/pokepocket/PokepocketApplication.java
@@ -1,4 +1,4 @@
-package com.pokepocket.pokepocket;
+package com.pokepocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
