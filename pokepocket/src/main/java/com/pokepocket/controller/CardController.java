07412f82ba24cb20ff0ab4fb81e4bb0e6813b063
MatthewJung14 <Matthewjung14@gmail.com>
2025-03-11 20:36:28 -0400

    Updated getCardImage using name and added instructions

--- pokepocket/src/main/java/com/pokepocket/controller/CardController.java
+++ pokepocket/src/main/java/com/pokepocket/controller/CardController.java
@@ -2,6 +2,7 @@ package com.pokepocket.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,9 +29,15 @@ public class CardController {
 
 
 
-    // @GetMapping("/{name}/image")
-    // public String getCardImage(@PathVariable String name) {
-    //     Card card = cardRepository.findByName(name).orElseThrow(() -> new RuntimeException("Card not found"));
+    @GetMapping("/{name}/image")
+    public String getCardImage(@PathVariable String name) {
+        Card card = cardRepository.findByName(name).orElseThrow(() -> new RuntimeException("Card not found"));
+        return card.getCardImage();
+    }
+
+    // @GetMapping("/{id}/image")
+    // public String getCardImage(@PathVariable Long id) {
+    //     Card card = cardRepository.findById(id).orElseThrow(() -> new RuntimeException("Card not found"));
     //     return card.getCardImage();
     // }
 
--- pokepocket/src/main/java/com/pokepocket/repository/CardRepository.java
+++ pokepocket/src/main/java/com/pokepocket/repository/CardRepository.java
@@ -1,10 +1,12 @@
 package com.pokepocket.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pokepocket.model.Card;
 
 public interface CardRepository extends JpaRepository<Card, Long>{
     Card findByCardId(Long cardId);
-    Card findByName(String name);
+    Optional<Card> findByName(String name);
 }
