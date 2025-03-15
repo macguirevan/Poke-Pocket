c35ddf2d30c71328430854408285634201c4d86d
MatthewJung14 <Matthewjung14@gmail.com>
2025-03-15 13:53:58 -0400

    Trade APIs created

--- pokepocket/src/main/java/com/pokepocket/controller/TradeController.java
+++ pokepocket/src/main/java/com/pokepocket/controller/TradeController.java
@@ -2,7 +2,9 @@ package com.pokepocket.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +27,26 @@ public class TradeController {
         return tradeRepository.findAll();
     }
 
+    // Get the contents of a specific trade
+    @GetMapping("/{tradeId}")
+    public Trade getTradeById(@PathVariable Long tradeId) {
+        return tradeRepository.findById(tradeId)
+                .orElseThrow(() -> new RuntimeException("Trade not found."));
+    }
+
+    // Get all trades by a user. 
+    @GetMapping("/user/{userId}")
+    public List<Trade> getTradesByUserId(@PathVariable Long userId) {
+        return tradeRepository.findByUser_UserId(userId);
+    }
+
     @PostMapping
     public Trade createCard(@RequestBody Trade trade) {
         return tradeRepository.save(trade);
+    }
+
+    @DeleteMapping("/{tradeId}")
+    public void deleteTrade(@PathVariable Long tradeId) {
+        tradeRepository.deleteById(tradeId);
     }
 }
