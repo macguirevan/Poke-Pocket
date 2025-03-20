b3956ec886241bf2d193e75252c9017cba42507d
MatthewJung14 <Matthewjung14@gmail.com>
2025-03-20 15:11:12 -0400

    API: Delete by Username method created

--- pokepocket/src/main/java/com/pokepocket/controller/UserController.java
+++ pokepocket/src/main/java/com/pokepocket/controller/UserController.java
@@ -3,7 +3,9 @@ package com.pokepocket.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,5 +48,10 @@ public class UserController {
         } else {
             return "Invalid username or password";
         }
+    }
+
+    @DeleteMapping("/{username}")
+    public void deleteUser(@PathVariable String username) {
+        userRepository.deleteByUsername(username);
     }
 }
