c5a665dbeb33b3f2a5fa3e2cfefa7abae65d9a30
danfranc0 <francodaniel2025@gmail.com>
2025-04-03 12:23:11 -0400

    avatar and drop down

---
+++ src/layout/Header/Avatar/Avatar.tsx
@@ -0,0 +1,60 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleUser } from '@fortawesome/free-regular-svg-icons'
+import styles from './Avatar.module.css'
+import { useState, useRef } from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Avatar() {
+  const [dropDown, setDropDown] = useState<boolean>(false);
+  const menu = useRef<HTMLDivElement>(null);
+  const menuBtn = useRef<HTMLDivElement>(null);
+
+  const toggleDrop = () => {
+    setDropDown(!dropDown);
+  }
+
+  const handleClickOutside = (e: MouseEvent) => {
+    if (menuBtn.current && menuBtn.current.contains(e.target as Node)) {
+      return;
+    }
+
+    if (menu.current && !menu.current.contains(e.target as Node) && dropDown) {
+      setDropDown(false);
+    }
+  }
+
+  document.addEventListener('mousedown', handleClickOutside);
+
+  return (
+      <div className={styles.avatarContainer}>
+        <div
+          className={styles.iconContainer}
+          ref={menuBtn}
+          onClick={toggleDrop}
+        >
+          <FontAwesomeIcon className={styles.icon} icon={faCircleUser} size="2x" />
+        </div>
+        {dropDown && (
+          <div className={styles.profileMenu} ref={menu}>
+            <ul>
+              <li>
+                <Link to="/listing" onClick={() => toggleDrop()}>
+                  Listings
+                </Link>
+              </li>
+              <li>
+                <Link to="/messages" onClick={() => toggleDrop()}>
+                  Messages
+                </Link>
+              </li>
+              <li>
+                <Link to="/logout" onClick={() => toggleDrop()}>
+                  Logout
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+  )
+}
